688f3559b22e35b1dbca46ee2c40c21d05b93a12
Leszek Milasz <leszekmilasz@gmail.com>
2018-10-09 11:01:16 +0200

    będzie pan zadowolony

--- MarkovSearch/MarkovSearch/Polynomial.cpp
+++ MarkovSearch/MarkovSearch/Polynomial.cpp
@@ -14,30 +14,23 @@ Polynomial::Polynomial(const double& a, const double& b, const double& c, const 
 	
 }
 
-void Polynomial::createDerivative()
+const Polynomial& Polynomial::createDerivative()
 {
-	derivativeNormPolynomial = new Polynomial(4 * a*a + e * e, 4 * a*b + 2 * d*e, 4 * c*c + e * e, 2 * b*e + 4 * c*d, 4 * a*e + 4 * c*e, b*b + d * d);
+	return Polynomial(4 * a*a + e * e, 4 * a*b + 2 * d*e, 4 * c*c + e * e, 2 * b*e + 4 * c*d, 4 * a*e + 4 * c*e, b*b + d * d);
 }
 
 const PairOfDoubles& Polynomial::getExtremumOnTriangle(const Triangle & triangle)
 {
 	PairOfDoubles pairOfDoubles;
+	pairOfDoubles.addCandidate(valueAtPoint(triangle.getPoint1));
+	pairOfDoubles.addCandidate(valueAtPoint(triangle.getPoint2));
+	pairOfDoubles.addCandidate(valueAtPoint(triangle.getPoint3));
 
 	return pairOfDoubles;
 }
 
-const PairOfDoubles& Polynomial::getExtremumDerivativeOnTriangle(const Triangle & triangle)
-{
-	return derivativeNormPolynomial->getExtremumOnTriangle(triangle);
-}
-
-Polynomial::~Polynomial()
-{
-	delete derivativeNormPolynomial;
-}
-
 double Polynomial::valueAtPoint(const Point & p)
-{
-	// TO DO dfsdfsdfsdfsdfsdfdsfsdf
-	return 0.0;
+{	
+	// a*p.x*p.x + b*p.x + c*p.y*p.y + d*p.y + e*p.x*p.y + f;
+	return a*p.x*p.x + b*p.x + c*p.y*p.y + d*p.y + e*p.x*p.y + f;
 }
--- MarkovSearch/MarkovSearch/Polynomial.h
+++ MarkovSearch/MarkovSearch/Polynomial.h
@@ -7,15 +7,12 @@ class Polynomial
 {
 private:
 	double a, b, c, d, e, f;
-	Polynomial *derivativeNormPolynomial;
 
 public:
 	//Polynomial form: a*x^2 + b*x + c*y^2 + d*y + e*x*y + f
 	Polynomial(const double& a, const double& b, const double& c, const double& d, const double& e, const double& f);
-	void createDerivative();
+	const Polynomial& createDerivative();
 	const PairOfDoubles& getExtremumOnTriangle(const Triangle& triangle);
-	const PairOfDoubles& getExtremumDerivativeOnTriangle(const Triangle& triangle);
-	virtual ~Polynomial();
 	double valueAtPoint(const Point& p);
 };
 
